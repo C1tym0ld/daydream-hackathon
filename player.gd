8231339cfcedbf75cb17715d709da6cc7564a159
C1tym0ld <james.tanaristy@gmail.com>
2025-09-28 11:29:03 -0500

    Finished 1st Draft

--- player.gd
+++ player.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
-const SPEED = 300.0        # Horizontal movement speed (pixels per second)
-const JUMP_VELOCITY = -350.0 # Vertical speed for the jump (negative is up)
+const SPEED = 500.0        # Horizontal movement speed (pixels per second)
+const JUMP_VELOCITY = -800.0 # Vertical speed for the jump (negative is up)
 const GRAVITY = 1000.0
-
+# Add this line at the top of your script (outside of any function)
+signal pressedYesOrNo(result: String)
 @warning_ignore("unused_parameter")
 func _process(delta: float) -> void:
-	if velocity.x != 0:
+	if velocity.y != 0:
+		$Sprite2D.animation="Jumping"
+	elif velocity.x != 0:
 		$Sprite2D.animation="Walk"
 	else:
 		$Sprite2D.animation="default"
@@ -15,10 +18,7 @@ func _process(delta: float) -> void:
 	if velocity.x > 0:
 		$Sprite2D.flip_h=false
 
-@onready var trigger = $"../Ghost/Area2D"
 func _physics_process(delta: float) -> void:
-	if trigger.overlaps_body($"."):
-		$"../Ghost/Label".show()
 	if not is_on_floor():                       
 		velocity.y += GRAVITY * delta
 		
@@ -38,3 +38,47 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0.0, SPEED * 10 * delta)
 		# Note: Multiplying by 10 * delta makes the deceleration fast
 	move_and_slide()
+
+@onready var trigger = $"../Ghost/Area2D"
+func _on_area_2d_body_entered(body: Node2D) -> void:
+		$"../Ghost/Label".text = "Hello Friend! What brings you here?" # Set your text first
+		$"../Ghost/Label".show() 
+		await get_tree().create_timer(2).timeout
+		$"../Player/Label".text = "I want to escape this dungeon!"
+		$"../Player/Label".show() 
+		await get_tree().create_timer(2).timeout
+		$"../Ghost/Label".text = "What makes you say that?"
+		await get_tree().create_timer(2).timeout
+		$"../Ghost/Label".text = "It’s so wonderful down here!"
+		$"../Player/Label".hide() 
+		await get_tree().create_timer(2).timeout
+		$"../Player/Label".text = "I have been here for 2 years"
+		$"../Player/Label".show() 
+		$"../Ghost/Label".hide()
+		await get_tree().create_timer(2.5).timeout
+		$"../Player/Label".text = "and you are the first interaction I’ve had since!"
+		await get_tree().create_timer(3.5).timeout
+		$"../Player/Label".hide()
+		await get_tree().create_timer(1.5).timeout
+		$"../Player/Label".text = "Surely you can help me return?"
+		$"../Player/Label".show()
+		await get_tree().create_timer(1.5).timeout
+		$"../Ghost/Label".text= "."
+		$"../Ghost/Label".show()
+		$"../Player/Label".hide()
+		await get_tree().create_timer(0.75).timeout
+		$"../Ghost/Label".text= ".."
+		await get_tree().create_timer(0.75).timeout
+		$"../Ghost/Label".text= "..."
+		await get_tree().create_timer(1.5).timeout
+		$"../Ghost/Label".text= "Fine."
+		await get_tree().create_timer(2.5).timeout
+		$"../Ghost/Label".text= "I’ll be generous this one time."
+		await get_tree().create_timer(2.5).timeout
+		$"../Ghost/Label".text= "I will open a path for you, but first, 
+		answer this question."
+		await get_tree().create_timer(2.5).timeout
+		$"../Ghost/Label".text= "To leave, all of us ghosts may die."
+		await get_tree().create_timer(2.5).timeout
+		$"../Ghost/Label".text= "Would you still like to leave?"
+		$VBoxContainer.show() # Example: Show your VBoxContainer with buttons
